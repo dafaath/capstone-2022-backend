f1080980570af75003ca722dbfd6ab4b20c737ee
dapaath <dafaathaullah123@gmail.com>
2022-05-09 13:45:10 +0700

    Mengupdate fitur authentication dengan register

--- app/routes/authentication.py
+++ app/routes/authentication.py
@@ -1,3 +1,10 @@
 from fastapi import APIRouter
 
+from app.utils.response import ResponseTemplate
+
 router = APIRouter(prefix="/authentications", tags=["Authentication"])
+
+
+@router.post("/register", status_code=201)
+def register():
+    return ResponseTemplate(201, "Registration successful")
