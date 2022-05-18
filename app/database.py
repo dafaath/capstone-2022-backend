1373007ad4c32180a09cb11e74ec511edf934960
dapaath <dafaathaullah123@gmail.com>
2022-05-18 10:35:22 +0700

    Menambahkan firestore sebagai depedency

--- app/database.py
+++ app/database.py
@@ -3,6 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 from config import get_settings
+from google.cloud import firestore
+
 
 settings = get_settings()
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.db_user}:{settings.db_pass}@{settings.db_host}:{settings.db_port}/{settings.db_name}"
@@ -14,6 +16,21 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+# The `project` parameter is optional and represents which project the client
+# will act on behalf of. If not supplied, the client falls back to the default
+# project inferred from the environment.
+import os
+
+
+def get_fs():
+    credentials = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                               '..', settings.google_application_credentials)
+    fs = firestore.Client(project=settings.google_cloud_project_id, credentials=credentials)
+    try:
+        yield fs
+    finally:
+        fs.close()
 
 
 def get_db():
