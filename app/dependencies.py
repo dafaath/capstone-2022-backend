3e9637a7bb0a65cc5fa50aed0f256e7a364f4cbb
dapaath <dafaathaullah123@gmail.com>
2022-06-08 19:07:55 +0700

    Menambahkan caching saat mengambil data dari model

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,27 +1,24 @@
 import os
 import pickle
 
+from functools import lru_cache
 import tensorflow as tf
 from keras.preprocessing.text import Tokenizer
 
 
+@lru_cache(maxsize=1)
 def get_tokenizer():
     tokenizer = None
     filename = 'tokenizer.pickle'
     full_path_filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'utils', filename)
     with open(full_path_filename, 'rb') as x:
         tokenizer: Tokenizer = pickle.load(x)
-    try:
-        yield tokenizer
-    finally:
-        pass
+    return tokenizer
 
 
+@lru_cache(maxsize=1)
 def get_model():
     filename = 'model.h5'
     full_path_filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'utils', filename)
     model = tf.keras.models.load_model(full_path_filename)
-    try:
-        yield model
-    finally:
-        pass
+    return model
